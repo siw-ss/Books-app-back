c23d846adf52c82499ed564ee3a8e0b52a8641a8
siw-ss <siwarsoula@gmail.com>
2023-12-21 23:21:38 +0100

    get books from DB with repository

--- src/main/java/com/example/demo/book/BookRepository.java
+++ src/main/java/com/example/demo/book/BookRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.book;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+
+
 }
--- src/main/java/com/example/demo/book/BookService.java
+++ src/main/java/com/example/demo/book/BookService.java
@@ -1,5 +1,6 @@
 package com.example.demo.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,10 +8,18 @@ import java.util.List;
 @Service
 public class BookService {
 
+    public final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     public List<Book> getBooks(){
-        return List.of(
-            new Book(1,"fault in our stars",200),
-            new Book(2,"dolphins",50)
-        );
+        return bookRepository.findAll();
+//        return List.of(
+//            new Book(1,"fault in our stars",200),
+//            new Book(2,"dolphins",50)
+//        );
     }
 }
